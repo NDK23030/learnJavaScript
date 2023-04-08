4c663a6e08d989ee221a575c98d9e5ae10cacb40
Denis <ggmazanovdenis@gmail.com>
2023-04-08 23:58:02 +0300

    Implementation of the script for sending data to the server

--- js/script.js
+++ js/script.js
@@ -204,7 +204,7 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         "img/tabs/elite.jpg",
         "elite",
-        'Меню “Премиум”"',
+        'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         14,
         '.menu .container',
@@ -214,11 +214,64 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         "img/tabs/post.jpg",
         "post",
-        'Меню "Постное""',
+        'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         21,
         '.menu .container',
         'menu__item'
     ).render();
+
+    //Forms request
+
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'Загрузка',
+            success: 'Спасибо! Скоро мы с вами свяжемся',
+            failure: 'Что-то пошло не так...'
+        }
+
+        forms.forEach(item => {
+            posData(item);
+        })
+
+        function posData(form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+
+                request.setRequestHeader('Content-type', 'application/json');
+                const formData = new FormData(form);
+
+                const object = {};
+                formData.forEach(function(value, key) {
+                    object[key] = value;
+                });
+
+                const json = JSON.stringify(object)
+
+                request.send(json);
+
+                request.addEventListener('load', ()=> {
+                    if (request.status === 200) {
+                        console.log(request.response);
+                        statusMessage.textContent = message.success;
+                        form.reset();
+                        setTimeout(() => {
+                            statusMessage.remove()
+                        }, 2000);
+                    } else {
+                        statusMessage.textContent = message.failure;
+                    }
+                });
+            });
+        }
 });
 
