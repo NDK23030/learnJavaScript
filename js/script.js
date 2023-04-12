a08d64c1a2e8f6490cfd501c1d3041e805f59201
Denis <ggmazanovdenis@gmail.com>
2023-04-13 00:41:11 +0300

    my first slider on JS

--- js/script.js
+++ js/script.js
@@ -321,5 +321,54 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+
+    // Slider 
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prevBtn = document.querySelector('.offer__slider-prev'),
+          nextBtn = document.querySelector('.offer__slider-next'),
+          totalSlides = document.querySelector('#total'),
+          currentSlide = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        totalSlides.textContent = `0${slides.length}`;
+    } else {
+        totalSlides.textContent = slides.length;
+    };
+        
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        };
+        if (n < 1) {
+            slideIndex =slides.length;
+        };
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            currentSlide.textContent = `0${slideIndex}`;
+        } else {
+            currentSlide.textContent = slideIndex;
+        };
+
+    }
+    
+    function plusSlide(n) {
+        showSlides(slideIndex += n)
+    }
+
+    nextBtn.addEventListener('click', () => {
+        plusSlide(1) 
+    });
+
+    prevBtn.addEventListener('click', () => {
+        plusSlide(-1)  
+    });
 });
 
